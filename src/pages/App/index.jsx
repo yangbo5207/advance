5ca11f07f05b832a06e9a6c1bb482f9fcec708c3
yang bo <yangbo@yangdeiMac.local>
2017-06-29 14:26:38 +0800

    update

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -3,16 +3,19 @@ import { HashRouter as Router, Route  } from 'react-router-dom';
 import Header from 'components/Header';
 
 import routers from './router';
+import './style.scss';
 
 class App extends Component {
     render() {
         return (
             <Router>
-                <div>
+                <div id="container">
                     <Header />
-                    {routers.map((router, i) => (
-                        <Route exact={router.exact ? true : false} path={router.path} component={router.component} key={i} />
-                    ))}
+                    <div className="main">
+                        {routers.map((router, i) => (
+                            <Route exact={router.exact ? true : false} path={router.path} component={router.component} key={i} />
+                        ))}
+                    </div>
                 </div>
             </Router>
         )
