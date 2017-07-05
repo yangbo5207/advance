6117ff7eba95fc6e292cc507fb61fb29ea3105d8
yang bo <yangbo@yangdeiMac.local>
2017-07-05 16:18:54 +0800

    update

--- src/pages/ReactCase/index.jsx
+++ src/pages/ReactCase/index.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 import Create from './Create';
 import routers from './router';
@@ -21,12 +20,10 @@ class Case extends Component {
                 </div>
 
                 <div className="react_case_content">
-                    <ReactCSSTransitionGroup transitionName="example" transitionAppear={true} transitionAppearTimeout="1500" transitionEnterTimeout={500}>
-                        <Route exact path={path} component={Create} />
-                        {routers.map((item, i) => (
-                            <Route path={`${path}${item.path}`} component={item.component} key={i} />
-                        ))}
-                    </ReactCSSTransitionGroup>
+                    <Route exact path={path} component={Create} />
+                    {routers.map((item, i) => (
+                        <Route path={`${path}${item.path}`} component={item.component} key={i} />
+                    ))}
                 </div>
             </div>
         )
